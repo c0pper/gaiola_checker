f6599898f7778bd4c8e3af14507c146afbf20df9
c0pper <93simonster@gmail.com>
2024-05-22 09:54:15 +0200

    disabled http logging

--- bot.py
+++ bot.py
@@ -27,7 +27,8 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-
+httpx_logger = logging.getLogger("httpx")
+httpx_logger.setLevel(logging.WARNING)
 
 @dataclass
 class Day:
