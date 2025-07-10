db5eaefc4728c0813889b767008038d8d37cbf12
93simon <93simonster@gmail.com>
2025-07-10 11:14:55 +0200

    fix: Correct birth date for Giuseppe Perrotta in people_data

--- src/data/people_data.py
+++ src/data/people_data.py
@@ -18,7 +18,8 @@ all_people = [ #  1993-09-04
     Person(name="Carmen", surname="Di Carlo", sex="F", bday="1997-07-09", cf="DCRCMN97L49B905M"),
     Person(name="Federica", surname="Antignano", sex="F", bday="1993-02-11", cf="NTGFRC93B51B905D"),
     Person(name="Carolina", surname="Carotenuto", sex="F", bday="1994-10-24", cf="CRTCLN94R64B905I"),
-    Person(name="Giuseppe", surname="Perrotta", sex="M", bday="1991-07-08", cf="PRRGPP91M07B905O"),
+    Person(name="Giuseppe", surname="Perrotta", sex="M", bday="1991-08-07", cf="PRRGPP91M07B905O"),
+    Person(name="Andrea", surname="Della Vecchia", sex="F", bday="1992-08-01", cf="DLLNDR92M41B905J"),  
     # Please provide the birth date for Manuela Marotta to complete her entry.
     # Person(name="Manuela", surname="Marotta", sex="F", bday="DD-MM-YYYY", cf="MTTMNL93D62F839C"),
 ]
